cfd7d1ef9503ff6122b6d49c6735c818a99beaea
Hammad Ahmad <hamadqur447@gmail.com>
2026-03-11 12:58:25 +0500

    updated

--- client/src/pages/landing-pages/LandingPageLayout.jsx
+++ client/src/pages/landing-pages/LandingPageLayout.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import Nav from '../../components/Nav'
 import { Outlet } from 'react-router-dom'
 import Footer from '../../components/Footer'
+import ad from "../../assets/images/ad.jpg"
 
 function LandingPageLayout() {
     return (
         <div>
             <Nav />
             <div className='px-20'>
-
+                <div className="advertisment flex justify-center items-center w-full py-10">
+                    <a href="#"><img src={ad} alt="" /></a>
+                </div>
                 <Outlet />
             </div>
             <Footer />
